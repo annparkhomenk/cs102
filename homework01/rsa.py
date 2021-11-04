65d54302398516c73b912688b44f153e61d620bb
Пархоменко Анна Андреевна <64017772+annparkhomenk@users.noreply.github.com>
2021-11-04 13:23:39 +0300

    Исправлены ошибки в оформлении rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -37,7 +37,7 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     n = p * q
-    phi = (p-1) * (q-1)
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
