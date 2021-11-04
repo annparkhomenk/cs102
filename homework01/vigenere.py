c5d6f39a68c2c5b74d20d7d776e05108d9abb59d
Пархоменко Анна Андреевна <64017772+annparkhomenk@users.noreply.github.com>
2021-11-04 13:10:47 +0300

    Перенесены изменения в vigenere.py в папке homework01

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,30 +1,29 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    for i, letter in enumerate(plaintext):
+        if letter.isalpha():
+            shift = ord(keyword[i % len(keyword)]) - ord('A')
+            if letter.islower():
+                ciphertext += chr((ord(letter) + shift - ord('a')) % 26 + ord('a'))
+            else:
+                ciphertext += chr((ord(letter) + shift - ord('A')) % 26 + ord('A'))
+        else:
+            ciphertext += letter
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    for i, letter in enumerate(ciphertext):
+        if letter.isalpha():
+            shift = ord(keyword[i % len(keyword)]) - ord('A')
+            if letter.islower():
+                plaintext += chr((ord(letter) - shift - ord('a')) % 26 + ord('a'))
+            else:
+                plaintext += chr((ord(letter) - shift - ord('A')) % 26 + ord('A'))
+        else:
+            plaintext += letter
+
     return plaintext
