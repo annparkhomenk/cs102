1ba7fca201909642ff9ad1ae26ff5dd554633f3a
Пархоменко Анна Андреевна <64017772+annparkhomenk@users.noreply.github.com>
2022-01-05 19:45:08 +0300

    Обновлен maze

--- homework04-new/maze.py
+++ homework04-new/maze.py
@@ -82,10 +82,7 @@ def get_exits(grid: List[List[Union[str, int]]]) -> List[Tuple[int, int]]:
     :param grid:
     :return:
     """
-    return [
-        (i, j) for i in range(len(grid))
-        for j in range(len(grid[0])) if grid[i][j] == "X"
-    ]
+    return [(i, j) for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == "X"]
 
 
 def make_step(grid: List[List[Union[str, int]]], k: int) -> List[List[Union[str, int]]]:
